ccea6d32fcd8f138254f12ae6e14f4cf1757d424
BinduMadamanchi <42659031+BinduMadamanchi@users.noreply.github.com>
2019-01-25 18:49:15 -0600

    Update reverse.py

--- ICP1/SOURCE/reverse.py
+++ ICP1/SOURCE/reverse.py
@@ -5,3 +5,10 @@ while(num>0):
     rev = (rev*10)+ rem
     num = num//10
 print("\n reverse of a number is: %d" %rev)
+
+
+''' Number = int(input("Enter any Number: "))
+Reverse = str(Number)[::-1]
+print("\n Reverse of entered number is = %s" %(Reverse)) '''
+
+
